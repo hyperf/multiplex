c3ed6fdf94f94b2ee1b8e7d6d83629f5b0613ba3
李铭昕 <715557344@qq.com>
2021-11-05 12:16:01 +0800

    Added test environment with swow.

---
+++ .github/swow.install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install libcurl4-openssl-dev
+wget https://github.com/swow/swow/archive/"${SW_VERSION}".tar.gz -O swow.tar.gz
+mkdir -p swow
+tar -xf swow.tar.gz -C swow --strip-components=1
+rm swow.tar.gz
+cd swow/ext || exit
+
+phpize
+./configure --enable-swow-debug --enable-swow-ssl --enable-swow-curl
+make -j "$(nproc)"
+sudo make install
+sudo sh -c "echo extension=swow > /etc/php/${PHP_VERSION}/cli/conf.d/swow.ini"
+php --ri swow
